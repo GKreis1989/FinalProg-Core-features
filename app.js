3b2bbed521b5fab3957aaaeb16cca8f4dd4af286
Noah Katz <noahkatz@gmail.com>
2023-11-23 09:55:15 -0500

    feat: added statically hosted files

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 import express from 'express';
 const app = express();
+
 import configRoutes from './routes/index.js';
 
+const staticDir = express.static(__dirname + '/public');
+const pagesDir = express.static(__dirname + '/pages');
+
+app.use('/public', staticDir);
+app.use('/', pagesDir);
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 configRoutes(app);
 
