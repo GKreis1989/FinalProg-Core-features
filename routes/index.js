7c2c0c8ee6ba8f2debdfff8d27de685aa8b8272a
GKreis1989 <148119278+GKreis1989@users.noreply.github.com>
2023-11-29 16:37:04 -0500

    Update index.js
    Changes to allow user signup.

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,33 @@ const constructorMethod = (app) => {
   app.use('/user', userRoutes);
   app.use('/auth', authRoutes);
 
+  app.post('/user/signup', async (req, res) => {
+    try {
+      const authResponse = await fetch("/auth/signup", {
+        method: "POST",
+        body: JSON.stringify(req.body),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+
+      if (authResponse.ok) {
+        const responseObject = await authResponse.json();
+        res.status(201).json(responseObject);
+      } 
+      
+      else {
+        console.error("Forwarded signup failed:", authResponse.statusText);
+        res.status(authResponse.status).send(authResponse.statusText);
+      }
+    } 
+    
+    catch (error) {
+      console.error("Error during signup forwarding:", error);
+      res.status(500).send('signup forwarding error');
+    }
+  });
+  
   app.get('/*', (req, res) => {
     res.redirect('/');
   })
