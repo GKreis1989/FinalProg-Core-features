43d45dafcffe35fcefeff690b6163b03ed5896fb
GKreis1989 <148119278+GKreis1989@users.noreply.github.com>
2023-11-29 14:58:09 -0500

    users.js
    users.js should now fetch users from servers and display them on the page.

--- public/js/users.js
+++ public/js/users.js
@@ -1,7 +1,16 @@
-const userList = $('userList');
+document.addEventListener('DOMContentLoaded', async () => {
+    const userList = document.getElementById('userList');
+    
+    try {
+        const response = await fetch('/user/all');
+        const users = await response.json();
 
-const users = undefined; // TODO: make call to server for users (maybe using fetch?)
-
-users.forEach(user => {
-    userList.appendChild(`<li>${user.name}</li>`);
+        users.forEach(user => {
+            const listItem = document.createElement('li');
+            listItem.textContent = `${user.firstName} ${user.lastName} - ${user.emailAddress}`;
+            userList.appendChild(listItem);
+        });
+    } catch (error) {
+        console.error('Error fetching users:', error);
+    }
 });
