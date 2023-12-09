08309551f5f881558109f85bdb9f419d88b2b316
Noah Katz <noahkatz@gmail.com>
2023-12-08 23:37:36 -0500

    fix: fixed medication caching; added granular search capabilities for medication searching

--- data/medication.js
+++ data/medication.js
@@ -1,12 +1,12 @@
 import { ObjectId } from "bson";
 import { medication as initMedication } from "../config/mongoCollections.js";
-import { CustomException, validateObjectId, validateString } from "../helpers.js";
+import { CustomException, validateObjectId, validateStringArray, validateString, validateMedicationSearchParam } from "../helpers.js";
 import axios from 'axios';
-
-export const medication = await initMedication();
 
 const BASE_URL = 'https://api.fda.gov/drug/ndc.json';
 const SEARCH_LIMIT = 200;
+
+// ! documentation: https://open.fda.gov/apis/drug/ndc/searchable-fields/
 
 const sampleMedicaiton = {
     _id: new ObjectId('654199d077b5d9aa7fedbf6e'),
@@ -24,7 +24,6 @@ const sampleMedicaiton = {
 }
 
 const paginatedEndpoint = async (endpoint, offset=0) => {
-    // TODO: error handling
     const initial = [];
     const response = await axios.get(`${endpoint}&skip=${offset}`);
     initial.push(...response.data.results);
@@ -33,42 +32,77 @@ const paginatedEndpoint = async (endpoint, offset=0) => {
     return initial;
 }
 
-export const searchMedications = async (keyword) => {
-    keyword = validateString('keyword', keyword);
-    const res = await paginatedEndpoint(`${BASE_URL}?search="${keyword}"&limit=${SEARCH_LIMIT}`);
+export const searchMedications = async (searchParam) => {
+    const searchString = validateMedicationSearchParam(searchParam);
+    const url = `${BASE_URL}?search=${searchString}&limit=${SEARCH_LIMIT}`
+    const res = await paginatedEndpoint(url);
     if(!res?.length) throw CustomException.notFound("medications with keyword", keyword);
-    return res;
+    const foundMedications = res.map(med => {
+        return {
+            productId: med?.product_id,
+            brandName: med?.brand_name,
+            dosageForm: med?.dosage_form,
+            route: med?.route,
+            genericName: med?.generic_name
+        }
+    })
+    return foundMedications;
 }
 
 export const getMedicationByProductId = async (productId) => {
+
+    const medication = await initMedication();
     productId = validateString('productId', productId);
+    const foundMedication = await medication.findOne({
+        'productId': productId
+    });
+    if(foundMedication?.hasOwnProperty('_id')) return foundMedication;
     const res = await paginatedEndpoint(`${BASE_URL}?search=product_id:"${productId}"&limit=${SEARCH_LIMIT}`);
     if(!res?.length) throw CustomException.notFound("medication with id", productId);
     if(res.length !== 1) throw CustomException.badParameter("productId");
-    return res[0];
+    const med = res[0];
+    return {
+        productId: med?.product_id,
+        brandName: med?.brand_name,
+        dosageForm: med?.dosage_form,
+        route: med?.route,
+        genericName: med?.generic_name
+    }
+
 };
 
 export const getMedicationByObjectId = async (medicationId) => {
+
+    const medication = await initMedication();
     const oId = validateObjectId('medicationId', medicationId);
     const foundMedication = await medication.findOne({ _id: oId });
     if(!foundMedication?._id) throw CustomException.notFound("medication with id ", medicationId);
+    foundMedication._id = foundMedication._id.toString();
     return foundMedication;
+
 }
 
 export const cacheMedication = async (productId) => {
-    const foundMedication = await getMedicationByProductId(productId);
-    const medicationObject = {
-        productId: foundMedication?.product_id,
-        brandName: foundMedication?.brand_name,
-        dosageForm: foundMedication?.dosage_form,
-        route: foundMedication?.route,
-        genericName: foundMedication?.generic_name,
-    }
-    Object.keys(medicationObject).forEach(field => {
+
+    const medication = await initMedication();
+    let foundMedication = await medication.findOne({
+        "productId": productId
+    });
+    if(foundMedication?.hasOwnProperty('_id')) 
+        throw CustomException.alreadyExists("cached medication with product id", productId);
+    foundMedication = await getMedicationByProductId(productId);
+    Object.keys(foundMedication).forEach(field => {
         try {
-            validateString(field, medicationObject[field]);
+            foundMedication[field] = (field === 'route' ? 
+                validateStringArray : validateString)(field, foundMedication[field]);
         } catch(e) {
-            medicationObject[field] = undefined;
+            foundMedication[field] = undefined;
         }
-    })
+    });
+    const createMedicationResponse  = await medication.insertOne(foundMedication);
+    if(createMedicationResponse.acknowledged) {
+        return await getMedicationByObjectId(createMedicationResponse.insertedId);
+    }
+    throw CustomException.serverError("caching medication");
+
 }
