523ea15d1da8ebfb7c5b4a551fc06d4e5e9c5203
Noah Katz <noahkatz@gmail.com>
2023-12-08 23:35:18 -0500

    refactor: removed RBAC from clinic creation

--- test/data/clinic.js
+++ test/data/clinic.js
@@ -1,43 +1,21 @@
 import { CustomException } from "../../helpers.js";
 import * as clinicData from "../../data/clinic.js";
-import * as userData from "../../data/user.js";
 import { clinic as initClinic } from "../../config/mongoCollections.js";
-import { user as initUser } from "../../config/mongoCollections.js";
 
 const main = async () => {
 
   let clinic = await initClinic();
-  let user = await initUser();
 
   const reset = async () => {
     await clinic.deleteMany({});
-    await user.deleteMany({});
   }
 
   await reset();
 
-  let id;
-
-  try {
-    const res = await userData.createUser(
-      'santa',
-      'clause',
-      'santa@stevens.edu',
-      'northpole',
-      'doctor'
-    );
-    id = res._id;
-  } catch(e) {
-    if(e instanceof CustomException) {
-      console.log(e.code, e.message);
-    }
-    else console.log(e);
-  }
-
   try {
     console.log('TESTING CREATE CLINIC');
     const res = await clinicData.createClinic(
-      'North Pole Clinic', id
+      'North Pole Clinic'
     );
     console.log(res);
   } catch(e) {
@@ -48,43 +26,6 @@ const main = async () => {
   }
 
   console.log();
-
-  try {
-    const res = await userData.updateUser({
-      userId: id.toString(),
-      role: 'admin'
-    });
-    id = res._id;
-  } catch(e) {
-    if(e instanceof CustomException) {
-      console.log(e.code, e.message);
-    }
-    else console.log(e);
-  }
-
-  try {
-    console.log('TESTING CREATE CLINIC');
-    const res = await clinicData.createClinic(
-      'North Pole Clinic', id
-    );
-    console.log(res);
-  } catch(e) {
-    if(e instanceof CustomException) {
-      console.log(e.code, e.message);
-    }
-    else console.log(e);
-  }
-
-  console.log();
-
-  try {
-    const res = await userData.removeUser(id);
-  } catch(e) {
-    if(e instanceof CustomException) {
-      console.log(e.code, e.message);
-    }
-    else console.log(e);
-  }
 
   await reset();
 
