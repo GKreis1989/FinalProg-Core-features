da7ea7d43b4641ecf27398e765f44f38e78ff711
GKreis1989 <148119278+GKreis1989@users.noreply.github.com>
2023-12-13 14:07:53 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,29 @@
-import userRoutes from './user.js';
+//import userRoutes from './user.js';
 
-const constructorMethod = (app) => {
-  app.use('/user', userRoutes);
+//const constructorMethod = (app) => {
+//  app.use('/user', userRoutes);
   
-  app.get('/*', (req, res) => {
-    res.redirect('/');
-  })
+ // app.get('/*', (req, res) => {
+ //   res.redirect('/');
+//  })
  
-  app.use('*', (req, res) => {
-    res.status(404).json({ error: 'Not found' });
-  });
-};
+//  app.use('*', (req, res) => {
+ //   res.status(404).json({ error: 'Not found' });
+//  });
+//};
   
-export default constructorMethod;
+//export default constructorMethod;
+
+//New changes
+import express from 'express';
+import addressRoutes from './address.js';
+import clinicRoutes from './clinic.js';
+import medicationRoutes from './medication.js';
+
+const router = express.Router();
+
+router.use('/address', addressRoutes);
+router.use('/clinic', clinicRoutes);
+router.use('/medication', medicationRoutes);
+
+export default router;
