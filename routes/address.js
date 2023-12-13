64dbdc6a5c89133f24ddab57eb934370232199a8
GKreis1989 <148119278+GKreis1989@users.noreply.github.com>
2023-12-13 13:48:57 -0500

    Update address.js

---
+++ routes/address.js
@@ -0,0 +1,59 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  try {
+    res.json({ message: 'Get all addresses route', data: addresses });
+  } 
+  
+  catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  const addressId = req.params.id;
+  try {
+    res.json({ message: `Get address by ID route for ID: ${addressId}`, data: address });
+  } 
+  
+  catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.post('/', async (req, res) => {
+  const newAddress = req.body;
+  try {
+    res.json({ message: 'Create address route', data: createdAddress });
+  } 
+  catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  const addressId = req.params.id;
+  const updatedAddress = req.body;
+  try {
+    res.json({ message: `Update address route for ID: ${addressId}`, data: updatedAddress });
+  } 
+  
+  catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  const addressId = req.params.id;
+  try {
+    res.json({ message: `Delete address route for ID: ${addressId}`, data: deletedAddress });
+  } 
+  
+  catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+export default router;
