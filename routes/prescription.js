7b488868fcc818953883ab9287d162f38bc4bc93
ChrisKehoe904 <145404960+ChrisKehoe904@users.noreply.github.com>
2023-12-14 20:20:35 -0500

    Update prescription.js
    added await to the createuserobject function

--- routes/prescription.js
+++ routes/prescription.js
@@ -8,10 +8,10 @@ const router = Router();
 router.route('/')
   .get(async (req, res) => {
     try {
-      const userObject = createUserObject(req.cookies.session);
+      const userObject = await createUserObject(req.cookies.session);
       res.status(200).json(prescriptions);
     } catch (error) {
-     const userObject = createUserObject(req.cookies.session);
+     const userObject = await createUserObject(req.cookies.session);
       console.error(error);
       if (error instanceof CustomException) res.status(error.code).json({ error: error.message });
       else res.status(500).json({ error: 'get prescriptions server error' });
@@ -20,7 +20,7 @@ router.route('/')
   .post(async (req, res) => {
     try {
       const { medicationId, quantity, unit, refills, startDate, endDate } = req.body;
-      const userObject = createUserObject(req.cookies.session);
+      const userObject = await createUserObject(req.cookies.session);
       const newPrescriptionId = await userObject.createPrescription(medicationId, quantity, unit, refills, startDate, endDate);
       res.status(200).json({ prescriptionId: newPrescriptionId });
     } catch (error) {
@@ -33,7 +33,7 @@ router.route('/')
 router.route('/:prescriptionId')
   .get(async (req, res) => {
     try {
-      const userObject = createUserObject(req.cookies.session);
+      const userObject = await createUserObject(req.cookies.session);
       const prescriptionId = req.params.prescriptionId;
       const prescription = await userObject.getPrescriptionById(prescriptionId);
       res.status(200).json(prescription);
@@ -45,7 +45,7 @@ router.route('/:prescriptionId')
   })
   .put('/:prescriptionId', async (req, res) => {
     try {
-      const userObject = createUserObject(req.cookies.session);
+      const userObject = await createUserObject(req.cookies.session);
       const prescriptionId = req.params.prescriptionId;
       const updatedPrescription = await userObject.updatePrescription({ prescriptionId, ...req.body });
       res.status(200).json(updatedPrescription);
@@ -57,7 +57,7 @@ router.route('/:prescriptionId')
   })
   .post('/assign-prescription/:patientId', async (req, res) => {
     try {
-      const userObject = createUserObject(req.cookies.session);
+      const userObject = await createUserObject(req.cookies.session);
       const { prescriptionId } = req.body;
       const patientId = req.params.patientId;
       const assignResponse = await userObject.assignPrescriptionToPatient(prescriptionId, patientId);
