c5acd82a2793c7e64d2fec1ba81ac74b659cbe42
Noah Katz <noahkatz@gmail.com>
2023-12-17 10:51:38 -0500

    added registration system for patients

--- middleware.js
+++ middleware.js
@@ -2,14 +2,26 @@ import * as helpers from './helpers.js';
 
 export const ROOT = async (req, res, next) => {
     const date = new Date();
-    console.log(`${date.toISOString()} ${req.method} ${req.originalUrl}`)
+    let user;
+    try {
+        user = helpers.authenticateUser(req);
+    } catch(err) {
+        user = { emailAddress: 'unauthenticated' }
+    }
+    console.log(`${date.toUTCString()} ${req.method} ${req.originalUrl} ${user.emailAddress}`)
     if(['/index.html', '/auth.html', '/user/login', '/user'].includes(req.originalUrl)
     || req.originalUrl.endsWith(".css") || req.originalUrl.endsWith(".js")) {
         return next();
     }
-    try {
-        helpers.authenticateUser(req);
-    } catch(error) {
+    if(user.emailAddress == 'unauthenticated') {
+        if(req.method == 'GET') {
+            if(['/index.html', '/auth.html'].includes(req.originalUrl)
+            || req.originalUrl.endsWith(".css") || req.originalUrl.endsWith(".js"))
+                return next();
+        }
+        else if(req.method == 'POST') {
+            if(['/user/login', '/user'].includes(req.originalUrl)) return next();
+        }
         return res.redirect('/auth.html');
     }
     next();
--- public/js/auth.js
+++ public/js/auth.js
@@ -4,44 +4,78 @@ const passwordInput = $id('password');
 const roleInput = $id('role');
 const lastNameInput = $id('lastName');
 const firstNameInput = $id('firstName');
-const confirmPasswordInput = $id('confirm-password')
+const confirmPasswordInput = $id('confirm-password');
+const genderInput = $id('gender');
+const dateOfBirthInput = $id('date-of-birth');
+const allergiesInput = $id('allergies');
 const switchAuthContent = $id('switch-auth-content');
 const switchAuthModeButton = $id('switch-auth-mode');
+const errorMessage = $id('error-message');
 const header = $id('header');
 
 let modeLogin = true;
 
+const requestLogin = async () => {
+    const authResponse = await fetch("/user/login", {
+        "method": "POST",
+        "body": JSON.stringify({
+            "emailAddress": emailAddressInput.value,
+            "password": passwordInput.value
+        }),
+        "headers": {
+            "content-type": "application/json"
+        }
+    });
+    return authResponse;
+}
+
+const requestRegister = async () => {
+    const authResponse = await fetch("/user", {
+        "method": "POST",
+        "body": JSON.stringify({
+            "emailAddress": emailAddressInput.value,
+            "role": roleInput.value,
+            "password": passwordInput.value,
+            "firstName": firstNameInput.value,
+            "lastName": lastNameInput.value
+        }),
+        "headers": {
+            "content-type": "application/json"
+        }
+    });
+    return authResponse;
+}
+
+const createPatient = async (user) => {
+    authResponse = await fetch("/patient", {
+        "method": "POST",
+        "body": JSON.stringify({
+            "user": user,
+            "allergies": allergiesInput.value.split(','),
+            "dateOfBirth": dateOfBirthInput.value,
+            "gender": genderInput.value,
+        }),
+        "headers": {
+            "content-type": "application/json"
+        }
+    });
+    return authResponse;
+}
+
 loginForm.addEventListener('submit', async (e) => {
     e.preventDefault();
-    let authResponse;
-    if(modeLogin) {
-        authResponse = await fetch("/user/login", {
-            "method": "POST",
-            "body": JSON.stringify({
-                "emailAddress": emailAddressInput.value,
-                "password": passwordInput.value
-            }),
-            "headers": {
-                "content-type": "application/json"
-            }
-        });
-    } else {
-        authResponse = await fetch("/user", {
-            "method": "POST",
-            "body": JSON.stringify({
-                "emailAddress": emailAddressInput.value,
-                "role": roleInput.value,
-                "password": passwordInput.value,
-                "firstName": firstNameInput.value,
-                "lastName": lastNameInput.value
-            }),
-            "headers": {
-                "content-type": "application/json"
-            }
-        });
-    }
-    const user = await authResponse.json();
+    let authResponse = await (modeLogin ? requestLogin : requestRegister)();
+    let user = await authResponse.json();
+    if(user.hasOwnProperty('role') && user.role == 'patient') user = await createPatient(user);
     if(user.hasOwnProperty('_id')) window.location = '/home.html';
+    else {
+        errorMessage.innerText = user.error;
+        errorMessage.classList.remove('hidden');
+        setTimeout(() => {
+            errorMessage.innerText = '';
+            errorMessage.classList.add('hidden');
+        }, 2000)
+    }
 })
 
 switchAuthModeButton.addEventListener('click', async (e) => {
@@ -58,5 +92,18 @@ switchAuthModeButton.addEventListener('click', async (e) => {
         })
         switchAuthModeButton.innerText = 'Login';
         header.innerText = 'Register';
+    }
+})
+
+
+roleInput.addEventListener('change', () => {
+    if(roleInput.value == 'patient') {
+        Array.from(document.getElementsByClassName('login-label-patient')).forEach(element => {
+            element.classList.remove('hidden');
+        })
+    } else {
+        Array.from(document.getElementsByClassName('login-label-patient')).forEach(element => {
+            element.classList.add('hidden');
+        })
     }
 })
